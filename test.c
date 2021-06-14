50c5775aa965298fc3dd2aa156b1adb32a57996c
ahmed195048 <66330570+ahmed195048@users.noreply.github.com>
2021-06-15 00:49:04 +0200

    test if book exist or not

---
+++ test.c
@@ -0,0 +1,22 @@
+int test(struct node **head,int val)
+{
+    t=0;
+    struct node *temp=*head;
+    if ((*head)->id==val)
+    {
+        return 1;
+    }
+    temp=*head;
+    while(temp->next!=NULL && temp->next->id!=val)
+    {
+        temp=temp->next;
+    }
+    if(temp->next==NULL)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
