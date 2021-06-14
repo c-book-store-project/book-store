70ad3f772fd7f3f9ef5628de3fa8fb0b8e6b72ee
ahmed195048 <66330570+ahmed195048@users.noreply.github.com>
2021-06-15 00:47:01 +0200

    write in file

---
+++ write.c
@@ -0,0 +1,8 @@
+void write(int value, char book_title[], char auther_writer[], char gener_type[], int year_a){
+FILE *f=fopen("pop.csv","a+");
+if(f==NULL)
+    printf("file not found");
+else{
+    fprintf(f,"%d,%s,%s,%s,%d\n",value,book_title,auther_writer,gener_type,year_a);
+}
+}
