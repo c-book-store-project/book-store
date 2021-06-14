deec212de6809ba8ee54342c261ae524d1a82700
ammar-moh <ammar195592@feng.bu.edu.eg>
2021-06-15 00:34:53 +0200

    Search Function & Display Function

---
+++ display.c
@@ -0,0 +1,25 @@
+void display(struct node *head)
+{
+    if(head==NULL)
+        printf("\n\n   * the list is empty *\n");
+    else
+    {
+        printf("\n\n \t\t\t                       * the list is *\n");
+        printf("                         ***** \n");
+    printf("%-20s%-20s %-20s %-20s %-20s\n","ID","TITLE","AUTHOR","GENER","YEAR OF PUPLICATION");
+    printf("-------------------------------------------------------------------------------------------------------------------\n");
+        while(head!=NULL)
+        {
+            printf("\n%-20d %-20s %-20s%-20s%-20d"
+                   ,head->id
+                   ,head->title
+                   ,head->auther_name
+                   ,head->gener
+                   ,head->year);
+            head=head->next;
+        }
+        printf("\n-----------------------------------------------------------------------------------------------------------------");
+
+        puts("\n");
+    }
+}
