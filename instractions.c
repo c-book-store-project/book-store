05899cc1407fd9b3cf5ff0b035ddc6d2e2829884
ahmed195048 <66330570+ahmed195048@users.noreply.github.com>
2021-06-16 20:08:25 +0200

    Update instractions.c

--- instractions.c
+++ instractions.c
@@ -9,7 +9,8 @@ int instr(void)
            "   3 - DELETE OF BOOK\n"
            "   4 - UPDATE BOOK DETAILES\n"
            "   5 - DISPLAY ALL BOOKS DETAILES\n"
-           "   6 - TO SAVE CHANGE\n");
+           "   6 - SELL BOOKS\n"
+           "   7 - TO SAVE CHANGE\n");
     unsigned int menuChoice;
     printf("%s", "? ");
     scanf("%u", &menuChoice);
