59c9be313f68350ca27bf99dd2a0f6bd67feb73c
ahmed271666 <66408218+ahmed271666@users.noreply.github.com>
2021-06-17 01:22:04 +0200

    Update test.c

--- test.c
+++ test.c
@@ -1,12 +1,10 @@
 int test(struct node **head,int val)
 {
-    t=0;
     struct node *temp=*head;
     if ((*head)->id==val)
     {
         return 1;
     }
-    temp=*head;
     while(temp->next!=NULL && temp->next->id!=val)
     {
         temp=temp->next;
